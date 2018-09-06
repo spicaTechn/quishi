0a6fb0f1b4cf7bd7f546293624728e3816e85455
Dibbya <dibyakarki65@gmail.com>
2018-09-06 17:00:59 +0545

    add index.blade.php and setup html

---
+++ resources/views/index.blade.php
@@ -0,0 +1,360 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/nice-select.css">
+        <link rel="stylesheet" href="css/custom.css">
+        <link rel="stylesheet" href="css/responsive.css">
+        <link rel="icon" href="images/fav-icon.png">
+        <title>QUISHI</title>
+    </head>
+    <body>
+        <header>
+            <div class="login-menu login-menu-xs">
+                <ul>
+                    <li class="nav-item"><a href="#" class="nav-link"> Sign In <i class="icon-power"></i></a></li>
+                    <li class="nav-item"><a href="#" class="nav-link"> Sign Up <i class="icon-user"></i></a></li>
+                </ul>
+            </div>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#"><img src="images/logo.png" class="img-fluid"></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item"><a href="#" class="nav-link">Blog</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link">Profiles</a></li>
+                            <!--  <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">construction</a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li> -->
+                        </ul>
+                    </div>
+                    <div class="login-menu login-menu-sm">
+                        <ul>
+                            <li class="nav-item"><a href="#" class="nav-link"> Sign In <i class="icon-power"></i></a></li>
+                            <li class="nav-item"><a href="#" class="nav-link"> Sign Up <i class="icon-user"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <!-- end header -->
+        <div class="banner-bg" style="background: url(images/banner.jpg) no-repeat center; background-size: cover;">
+            <div class="most-search-job">
+                <ul>
+                    <li>Graphics Designer</li>
+                    <li>Graphics Designer</li>
+                    <li>Graphics Designer</li>
+                    <li>Graphics Designer</li>
+                    <li>Graphics Designer</li>
+                    <li>Graphics Designer</li>
+                    <li>Graphics Designer</li>
+                    <li>Graphics Designer</li>
+                    <li>Graphics Designer</li>
+                    <li>Graphics Designer</li>
+                    <li>Graphics Designer</li>
+                </ul>
+            </div>
+            <div class="container">
+                <div class="search-absolute-content text-center">
+                    <h1>search</h1>
+                    <form>
+                        <div class="search-wrapper">
+
+                            <div class="form-group">
+                                <i class="icon-magnifier"></i>
+                                <input type="text" name="" class="form-control" placeholder="Search keyword or title">
+                            </div>
+                            <div class="form-group">
+                                <i class="icon-location-pin"></i>
+                                <input type="text" name="" class="form-control" placeholder="Location" style="background: #f4f4f4;">
+                            </div>
+
+                            <button class="btn btn-transpatent"><i class="icon-magnifier"></i></button>
+
+                        </div>
+                    </form>
+                    <p>Your career guide with sincere and honest tales of experiences from professionals around the world.</p>
+                </div>
+            </div>
+        </div>
+        <div class="page-section trending-profiles">
+            <div class="container">
+                <div class="section-title">
+                    <h2>trending profiles</h2>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="trending-profiles-section">
+                            <div class="profile-image">
+                                <img src="images/profile/1.jpg">
+                            </div>
+                            <div class="profile-desination">
+                                <h3>Felicity Smoak</h3>
+                                <span>UI/UX Designer</span>
+                            </div>
+                            <div class="profile-slills">
+                                <ul>
+                                    <li><a href="#">CSS</a></li>
+                                    <li><a href="#">jQuery</a></li>
+                                    <li><a href="#">HTML5</a></li>
+                                </ul>
+                            </div>
+                            <div class="profile-info">
+                                <p>Felicity is a dedicated UI/UX designer for web and mobile applications.</p>
+                            </div>
+                            <div class="like-view">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="view-section">
+                                            <a href="#"><i class="icon-like"></i></a>
+                                            <span>10k Likes</span>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="view-section">
+                                            <a href="#"><i class="icon-eye"></i></a>
+                                            <span>10k Views</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="view-profile">
+                                <a href="#">view profile</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="trending-profiles-section">
+                            <div class="profile-image">
+                                <img src="images/profile/2.jpg">
+                            </div>
+                            <div class="profile-desination">
+                                <h3>Felicity Smoak</h3>
+                                <span>UI/UX Designer</span>
+                            </div>
+                            <div class="profile-slills">
+                                <ul>
+                                    <li><a href="#">CSS</a></li>
+                                    <li><a href="#">jQuery</a></li>
+                                    <li><a href="#">HTML5</a></li>
+                                </ul>
+                            </div>
+                            <div class="profile-info">
+                                <p>Felicity is a dedicated UI/UX designer for web and mobile applications.</p>
+                            </div>
+                            <div class="like-view">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="view-section">
+                                            <a href="#"><i class="icon-like"></i></a>
+                                            <span>10k Likes</span>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="view-section">
+                                            <a href="#"><i class="icon-eye"></i></a>
+                                            <span>10k Views</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="view-profile">
+                                <a href="#">view profile</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="trending-profiles-section">
+                            <div class="profile-image">
+                                <img src="images/profile/3.jpg">
+                            </div>
+                            <div class="profile-desination">
+                                <h3>Felicity Smoak</h3>
+                                <span>UI/UX Designer</span>
+                            </div>
+                            <div class="profile-slills">
+                                <ul>
+                                    <li><a href="#">CSS</a></li>
+                                    <li><a href="#">jQuery</a></li>
+                                    <li><a href="#">HTML5</a></li>
+                                </ul>
+                            </div>
+                            <div class="profile-info">
+                                <p>Felicity is a dedicated UI/UX designer for web and mobile applications.</p>
+                            </div>
+                            <div class="like-view">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="view-section">
+                                            <a href="#"><i class="icon-like"></i></a>
+                                            <span>10k Likes</span>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="view-section">
+                                            <a href="#"><i class="icon-eye"></i></a>
+                                            <span>10k Views</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="view-profile">
+                                <a href="#">view profile</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="view-more text-center"><a href="#" class="btn btn-default">view more</a></div>
+            </div>
+        </div>
+        <!-- trending-profiles -->
+        <div class="page-section the-media">
+            <div class="container">
+                <div class="section-title">
+                    <h2>In the Media</h2>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="home-blog-section">
+                            <div class="blog-image">
+                                <img src="images/blog1.jpg" alt="#">
+                            </div>
+                            <div class="blog-conten">
+                                <h4>Praesent malesuada semi to.</h4>
+                                <span class="time">Eva Marcel on 24th August</span>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra pellentesque quam. Praesent pulvinar in erat facilisis aliquam</p>
+                                <a href="#">Full Story <i class="icon-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="home-blog-section">
+                            <div class="blog-image">
+                                <img src="images/blog2.jpg" alt="#">
+                            </div>
+                            <div class="blog-conten">
+                                <h4>Praesent malesuada semi to.</h4>
+                                <span class="time">Eva Marcel on 24th August</span>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra pellentesque quam. Praesent pulvinar in erat facilisis aliquam</p>
+                                <a href="#">Full Story <i class="icon-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="view-more text-center"><a href="#" class="btn btn-default">all blogs</a></div>
+            </div>
+        </div>
+        <!-- trending-profiles -->
+        <div class="page-section about-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="about-inner-section">
+                            <div class="about-icon-section">
+                                <img src="images/icons/expert.png" alt="expert">
+                            </div>
+                            <div class="about-content-section">
+                                <h4>TALK TO AN EXPERT</h4>
+                                <p>Suspendisse sollicitudin tincidunt ex, vitae porta ante pretium a. Vestibulum ultricies velit urna, id eleifend tellus iaculis ac. Nullam tristique sagittis magna id eleifend.</p>
+                                <a href="#">More about our experts <i class="icon-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end col -->
+                    <div class="col-md-4">
+                        <div class="about-inner-section">
+                            <div class="about-icon-section">
+                                <img src="images/icons/career-review.png" alt="career-review">
+                            </div>
+                            <div class="about-content-section">
+                                <h4>CAREER REVIEWS</h4>
+                                <p>Suspendisse sollicitudin tincidunt ex, vitae porta ante pretium a. Vestibulum ultricies velit urna, id eleifend tellus iaculis ac. Nullam tristique sagittis magna id eleifend.</p>
+                                <a href="#">More about our experts <i class="icon-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end col -->
+                    <div class="col-md-4">
+                        <div class="about-inner-section">
+                            <div class="about-icon-section">
+                                <img src="images/icons/about-quishi.png" alt="quishi icon">
+                            </div>
+                            <div class="about-content-section">
+                                <h4>ABOUT US</h4>
+                                <p>Suspendisse sollicitudin tincidunt ex, vitae porta ante pretium a. Vestibulum ultricies velit urna, id eleifend tellus iaculis ac. Nullam tristique sagittis magna id eleifend.</p>
+                                <a href="#">More about our experts <i class="icon-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end col -->
+                </div>
+            </div>
+        </div>
+        <!-- end about section -->
+        <footer class="footer">
+            <div class="main-footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="footer-section">
+                                <h4>Address</h4>
+                                <p>47  Hardesty Street,<br>
+                                    North Greenbush<br>
+                                    New York<br>
+                                USA</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="footer-section">
+                                <h4>Contact</h4>
+                                <p>Phone: <a href="callto:5184617960">518-461-7960</a><br>Email: <a href="mailto:quishi@quishi.com">quishi@quishi.com</a> <br>
+                                <a href="mailto:quishi@outlook.com">quishi@outlook.com</a></p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="footer-section">
+                                <h4>Donate</h4>
+                                <div class="donate-image">
+                                    <img src="images/paypal.png" alt="paypals">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer-social-media">
+                        <ul>
+                            <li><a href="#"><i class="icon-social-facebook"></i></a></li>
+                            <li><a href="#"><i class="icon-social-twitter"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright">
+                <div class="container">
+                    <div class="copyright text-center">
+                        &copy; 2018 Quishi. All rights reserved.
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.nice-select.min.js"></script>
+        <script src="js/custom.js"></script>
+    </body>
+</html>
