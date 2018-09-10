e7949fdcd7ca9ffb86243724ad193bd0fe3f8bfe
dahalsuman <dahalsuman85@gmail.com>
2018-09-10 12:42:05 +0545

    made changes to the structrue of the admin layout

---
+++ quishi_source_code/resources/views/admin/inc/head.blade.php
@@ -0,0 +1,44 @@
+<title>Quishi - Superadmin</title>
+<!-- HTML5 Shim and Respond.js IE10 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 10]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<!-- Meta -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="description" content="#">
+<meta name="keywords" content="Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
+<meta name="author" content="#">
+<!-- Favicon icon -->
+<link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
+<!-- Google font-->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
+<!-- Required Fremwork -->
+<link rel="stylesheet" type="text/css" href="bower_components/bootstrap/css/bootstrap.min.css">
+<!-- themify-icons line icon -->
+<link rel="stylesheet" type="text/css" href="assets/icon/themify-icons/themify-icons.css">
+<!-- ico font -->
+<link rel="stylesheet" type="text/css" href="assets/icon/icofont/css/icofont.css">
+<!-- Menu-Search css -->
+<link rel="stylesheet" type="text/css" href="assets/pages/menu-search/css/component.css">
+<!--pagewise css-->
+
+<!--Datatable CSS-->
+<link rel="stylesheet" type="text/css" href="bower_components/datatables.net-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" type="text/css" href="assets/pages/data-table/css/buttons.dataTables.min.css">
+<link rel="stylesheet" type="text/css" href="bower_components/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css">
+<link rel="stylesheet" type="text/css" href="assets/pages/data-table/extensions/buttons/css/buttons.dataTables.min.css">
+<!--End Datatable CSS-->
+
+<!--Sweetalert enque all the pages except dashboard-->
+<link rel="stylesheet" type="text/css" href="bower_components/sweetalert/css/sweetalert.css">
+<!-- end sweetalert -->
+<!--Formvalidation-->
+<link rel="stylesheet" type="text/css" href="bower_components/formvalidation/formValidation.min.css">
+<!--End formvalidation-->
+<!-- Style.css -->
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<link rel="stylesheet" type="text/css" href="assets/css/jquery.mCustomScrollbar.css">
