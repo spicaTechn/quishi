65625cee96abf3e5ed640c311c270cfce5fc164c
spicaTechn <spicatechnos@gmail.com>
2018-09-10 10:28:21 +0545

    removing the /public from the url and creating the new folder structure to match the .htaccess

--- resources/views/front/inc/head.blade.php
+++ resources/views/front/inc/head.blade.php
@@ -2,9 +2,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/nice-select.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
-    <link rel="icon" href="{{ asset('images/fav-icon.png') }}">
+    <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/nice-select.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/custom.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/responsive.css') }}">
+    <link rel="icon" href="{{ asset('/images/fav-icon.png') }}">
+    <p>{{asset('/')}}</p>
     <title>QUISHI</title>
