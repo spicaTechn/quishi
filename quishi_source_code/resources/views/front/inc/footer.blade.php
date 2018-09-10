68aa8ac6c3b886b6a6cf5ae978176b3920bbec94
Dibbya <dibyakarki65@gmail.com>
2018-09-10 15:42:16 +0545

    Correction in footer.blade.php

--- quishi_source_code/resources/views/front/inc/footer.blade.php
+++ quishi_source_code/resources/views/front/inc/footer.blade.php
@@ -1,3 +1,50 @@
+
+<footer class="footer">
+            <div class="main-footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="footer-section">
+                                <h4>Address</h4>
+                                <p>47  Hardesty Street,<br>
+                                    North Greenbush<br>
+                                    New York<br>
+                                USA</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="footer-section">
+                                <h4>Contact</h4>
+                                <p>Phone: <a href="callto:5184617960">518-461-7960</a><br>Email: <a href="mailto:quishi@quishi.com">quishi@quishi.com</a> <br>
+                                <a href="mailto:quishi@outlook.com">quishi@outlook.com</a></p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="footer-section">
+                                <h4>Donate</h4>
+                                <div class="donate-image">
+                                    <img src="images/paypal.png" alt="paypals">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer-social-media">
+                        <ul>
+                            <li><a href="#"><i class="icon-social-facebook"></i></a></li>
+                            <li><a href="#"><i class="icon-social-twitter"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright">
+                <div class="container">
+                    <div class="copyright text-center">
+                        &copy; 2018 Quishi. All rights reserved.
+                    </div>
+                </div>
+            </div>
+        </footer>
+
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="{{ asset('/front/js/bootstrap.min.js') }}"></script>
