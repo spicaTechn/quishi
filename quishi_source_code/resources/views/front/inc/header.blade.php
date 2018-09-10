9cab012ac8375afe5d1f7121e66822f53dbcb16c
spicaTechn <spicatechnos@gmail.com>
2018-09-10 17:57:31 +0545

    updated the lateset css and include the link and changes the images path in the front/inc/header.blade.php

--- quishi_source_code/resources/views/front/inc/header.blade.php
+++ quishi_source_code/resources/views/front/inc/header.blade.php
@@ -8,15 +8,15 @@
     </div>
     <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="images/logo.png" class="img-fluid"></a>
+            <a class="navbar-brand" href="{{URL::to('/')}}"><img src="{{asset('/front/images/logo.png')}}" class="img-fluid"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item"><a href="#" class="nav-link">Blog</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Blog</a></li>
+                    <li class="nav-item"><a href="{{URL::to('/about')}}" class="nav-link">About</a></li>
+                    <li class="nav-item"><a href="{{URL::to('/contact')}}" class="nav-link">Contact</a></li>
                     <li class="nav-item"><a href="#" class="nav-link">Profiles</a></li>
                     <!--  <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">construction</a>
@@ -31,8 +31,13 @@
             </div>
             <div class="login-menu login-menu-sm">
                 <ul>
-                    <li class="nav-item"><a href="#" class="nav-link"> Sign In <i class="icon-power"></i></a></li>
-                    <li class="nav-item"><a href="#" class="nav-link"> Sign Up <i class="icon-user"></i></a></li>
+                    @guest
+                        <li class="nav-item"><a href="{{asset('/login')}}" class="nav-link"> Sign In <i class="icon-power"></i></a></li>
+                        <li class="nav-item"><a href="{{asset('/register')}}" class="nav-link"> Sign Up <i class="icon-user"></i></a></li>
+                        
+                    @else
+                        <li class="nav-item logdin"><a href="#" class="nav-link"><img src="{{asset('/front')}}/images/blog1.jpg"> Hi {{auth()->user()->name}} </a></li>
+                    @endguest
                 </ul>
             </div>
         </div>
