12e965f3b708b2d2e3fa3f29ecd06a674962b2ef
laxmantako12 <laxmantako12@gmail.com>
2018-11-16 17:23:42 +0545

    edit blog section

--- quishi_source_code/resources/views/front/career-advisor/blog/add.blade.php
+++ quishi_source_code/resources/views/front/career-advisor/blog/add.blade.php
@@ -3,51 +3,54 @@
 @section('content')
 <div class="profile-blog-edit-page profile-main-section">
 	<form>
-	<div class="editable-title">
-		<h2>According to the culture</h2>
-	</div>
-	<div class="editable-content">
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	</div>
+		<div class="row">
+		<div class="col-md-8 col-lg-6">
+			<div class="editable-title">
+				<h5>Title</h5>
+				<div class="form-group">
+					<input class="form-control" placeholder="According to the culture">
+				</div>
+			</div>
+			<div class="editable-content">
+				<h5>Description</h5>
+				<textarea class="text-editor"></textarea>
+			</div>
 
-	<div class="abstract">
-		hello there, I am the abstract
-	</div>
-	<div class="published-date">
-		<label>Published Date: </label>
-		<div id="datepicker" class="input-group date" data-date-format="mm-dd-yyyy">
-		    <input class="form-control" type="text" readonly />
-		    <span class="input-group-addon"><i class="icon-calendar"></i></span>
+			<div class="abstract">
+				<h5>Abstract</h5>
+				<textarea class="form-control" placeholder="hello there, I am the abstract"></textarea>
+			</div>
 		</div>
-	</div>
-	<div class="featured-image">
-		<h5>Featured Image</h5>
+		<div class="col-md-4 col-lg-4">
+				
+				<div class="published-date">
+					<label>Published Date: </label>
+					<div id="datepicker" class="input-group date" data-date-format="mm-dd-yyyy">
+					    <input class="form-control" type="text" readonly />
+					    <span class="input-group-addon"><i class="icon-calendar"></i></span>
+					</div>
+				</div>
+				<div class="featured-image">
+					<h5>Featured Image</h5>
 
-		<div class="avatar-upload">
-        <div class="avatar-edit">
-            <input type='file' id="imageUpload" accept=".png, .jpg, .jpeg" />
-            <label for="imageUpload"></label>
-        </div>
-        <div class="avatar-preview">
-            <div id="imagePreview" style="background-image: url(http://i.pravatar.cc/500?img=7);">
-            </div>
-        </div>
-    </div>
-	</div>
-	<div class="form-group">
-		<button class="btn btn-default">Update</button>
-	</div>
+					<div class="avatar-upload">
+			        <div class="avatar-edit">
+			            <input type='file' id="imageUpload" accept=".png, .jpg, .jpeg" />
+			            <label for="imageUpload"></label>
+			        </div>
+			        <div class="avatar-preview">
+			            <div id="imagePreview" style="background-image: url(http://i.pravatar.cc/500?img=7);">
+			            </div>
+			        </div>
+			    </div>
+				</div>
+				<div class="form-group">
+					<button class="btn btn-default">Update</button>
+				</div>
+		</div>
+	
+</div>
+	
 </form>
 
 </div>
@@ -57,6 +60,7 @@
 @endsection
 @section('page_specific_js')
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
+<script type="text/javascript" src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=qagffr3pkuv17a8on1afax661irst1hbr4e6tbv888sz91jc"></script>
     <script>
 
 		$(function () {
@@ -79,6 +83,22 @@
 		}
 		$("#imageUpload").change(function() {
 		    readURL(this);
+		});
+
+		//tinymce
+		tinymce.init({
+		  selector: 'textarea.text-editor',
+		  height: 400,
+		  menubar: true,
+		  plugins: [
+		    'fullpage advlist autolink lists link image charmap print preview anchor',
+		    'searchreplace visualblocks advcode fullscreen',
+		    'insertdatetime media table contextmenu'
+		  ],
+		  toolbar: 'code fullpage',
+		  content_css: [
+		    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+		    '//www.tinymce.com/css/codepen.min.css']
 		});
     	
         
