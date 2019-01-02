beac92ca2f8eb0431e4d2fd49f6a058ec9beaded
laxmantako12 <laxmantako12@gmail.com>
2019-01-02 15:55:17 +0545

    create html for following and followers

--- quishi_source_code/resources/views/front/career-advisor/followers/index.blade.php
+++ quishi_source_code/resources/views/front/career-advisor/followers/index.blade.php
@@ -3,6 +3,144 @@
 @section('page_specific_css')
 @endsection
 @section('content')
+<div class="follows-section profile-main-section">
+    <h4>Follows</h4>
+    <div class="follower-wrap">
+        <div class="row">
+            <div class="col-lg-3 col-md-4">
+                <div class="trending-profiles-section">
+                    <div class="profile-image">
+                        <img src="images/profile/2.jpg">
+                    </div>
+                    <div class="profile-desination">
+                        <h3>Felicity Smoak</h3>
+                        <span>UI/UX Designer</span>
+                    </div>
+                    <div class="like-view">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="view-section">
+                                    <a href="#"><i class="icon-like"></i></a>
+                                    <span>10k Likes</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="view-section">
+                                    <a href="#"><i class="icon-eye"></i></a>
+                                    <span>10k Views</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="view-profile row">
+                        <a href="#" class="follow_career_advisor">Follow</a>
+                        <a href="#">view profile</a>
+                    </div>
+                </div>
+            </div>
+            <!-- end col -->
+            <div class="col-lg-3 col-md-4">
+                <div class="trending-profiles-section">
+                    <div class="profile-image">
+                        <img src="images/profile/2.jpg">
+                    </div>
+                    <div class="profile-desination">
+                        <h3>Felicity Smoak</h3>
+                        <span>UI/UX Designer</span>
+                    </div>
+                    <div class="like-view">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="view-section">
+                                    <a href="#"><i class="icon-like"></i></a>
+                                    <span>10k Likes</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="view-section">
+                                    <a href="#"><i class="icon-eye"></i></a>
+                                    <span>10k Views</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="view-profile row">
+                        <a href="#" class="follow_career_advisor">Follow</a>
+                        <a href="#">view profile</a>
+                    </div>
+                </div>
+            </div>
+            <!-- end col -->
+            <div class="col-lg-3 col-md-4">
+                <div class="trending-profiles-section">
+                    <div class="profile-image">
+                        <img src="images/profile/2.jpg">
+                    </div>
+                    <div class="profile-desination">
+                        <h3>Felicity Smoak</h3>
+                        <span>UI/UX Designer</span>
+                    </div>
+                    <div class="like-view">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="view-section">
+                                    <a href="#"><i class="icon-like"></i></a>
+                                    <span>10k Likes</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="view-section">
+                                    <a href="#"><i class="icon-eye"></i></a>
+                                    <span>10k Views</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="view-profile row">
+                        <a href="#" class="follow_career_advisor">Follow</a>
+                        <a href="#">view profile</a>
+                    </div>
+                </div>
+            </div>
+            <!-- end col -->
+            <div class="col-lg-3 col-md-4">
+                <div class="trending-profiles-section">
+                    <div class="profile-image">
+                        <img src="images/profile/2.jpg">
+                    </div>
+                    <div class="profile-desination">
+                        <h3>Felicity Smoak</h3>
+                        <span>UI/UX Designer</span>
+                    </div>
+                    <div class="like-view">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="view-section">
+                                    <a href="#"><i class="icon-like"></i></a>
+                                    <span>10k Likes</span>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="view-section">
+                                    <a href="#"><i class="icon-eye"></i></a>
+                                    <span>10k Views</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="view-profile row">
+                        <a href="#" class="follow_career_advisor">Follow</a>
+                        <a href="#">view profile</a>
+                    </div>
+                </div>
+            </div>
+            <!-- end col -->
+        </div>
+        <!-- end row -->
+    </div>
+    <!-- end follower-wrap -->
+</div>
+<!-- end follows-section -->
 @endsection
 @section('page_specific_js')
 @endsection
