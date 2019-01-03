a2fa3404cb7c74f53fa23b243c609e8a81fd664f
laxmantako12 <laxmantako12@gmail.com>
2019-01-03 18:59:47 +0545

    final changes of css

--- quishi_source_code/resources/views/front/career-advisor/inc/footer.blade.php
+++ quishi_source_code/resources/views/front/career-advisor/inc/footer.blade.php
@@ -169,6 +169,7 @@
 
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
 <script src="{{ asset('/front/js/bootstrap.min.js') }}"></script>
 <!-- <script src="{{ asset('/front/js/jquery-nice-select.min.js') }}"></script> -->
 <!-- <script src="{{ asset('/front/js/jquery.nice-select.min.js') }}"></script> -->
